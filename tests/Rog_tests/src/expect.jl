1e17544a79653c7d08fc4e76b74b95c51ce83736
zlaraib <zlaraib@vols.utk.edu>
2023-06-27 02:50:11 -0400

    added comments

--- tests/Rog_tests/src/expect.jl
+++ tests/Rog_tests/src/expect.jl
@@ -1,20 +1,38 @@
 include("gates_function.jl")  # Include the gates_functions.jl file
 
 function calc_expect(s, tau, N, cutoff, ttotal)
+    # Initialize psi to be a product state (alternating up and down)
     psi = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
 
+    # center site because the Sz operator can be arbitralily chosen to be placed at this site at each time step
+    # to match up with Rog results we changed the placement of Sz operator on first site instead of middle.
     c = div(N, 2)
+
+    # Create empty array to store sz values 
     Sz_array = Float64[]
+    # Create empty array to store survival probability values 
     prob_surv_array = Float64[]
 
+    # extract the gates array generated in the gates_function file
     gates = create_gates(s, N, tau)
 
+    # Compute and print survival probability (found from <Sz>) at each time step then apply the gates to go to the next time
     for t in 0.0:tau:ttotal
+        # compute initial expectation value of Sz(inbuilt operator in ITensors library) at the first site on the chain
         sz = expect(psi, "Sz"; sites=1)
+        # add an element sz to the end of Sz array 
         push!(Sz_array, sz)
 
+        # Writing an if statement in a shorthand way that checks whether the current value of t is equal to ttotal, 
+        # and if so, it executes the break statement, which causes the loop to terminate early.
         t ≈ ttotal && break
+
+        # apply each gate in gates successively to the wavefunction psi (it is equivalent to time evolving psi according to the time-dependent Hamiltonian represented by gates).
+        # The apply function is smart enough to determine which site indices each gate has, and then figure out where to apply it to our MPS. 
+        # It automatically handles truncating the MPS and handles the non-nearest-neighbor gates in this example.
         psi = apply(gates, psi; cutoff)
+        # The normalize! function is used to ensure that the MPS is properly normalized after each application of the time evolution gates. 
+        # This is necessary to ensure that the MPS represents a valid quantum state.
         normalize!(psi)
 
         prob_surv = 0.5 * (1 - 2 * sz)
--- tests/Rog_tests/src/gates_function.jl
+++ tests/Rog_tests/src/gates_function.jl
@@ -1,20 +1,33 @@
 using ITensors
 
 function create_gates(s, N, tau)
+    # Make gates (1,2),(2,3),(3,4),... i.e. unitary gates which act on any (non-neighboring) pairs of sites in the chain.
+    # Create an empty ITensors array that will be our Trotter gates
     gates = ITensor[]
 
     for i in 1:(N-1)
         for j in i+1:N
+            #s1, s2 are non-neighbouring spin site/indices from the s array
             s1 = s[i]
             s2 = s[j]
+            # Our neutrino system Hamiltonian of self-interaction term represents 1D Heisenberg model.
+            # total Hamiltonian of the system is a sum of local terms hj, where hj acts on sites i and j which are paired for gates to latch onto.
+            # op function returns these operators as ITensors and we tensor product and add them together to compute the operator hj.
             hj = 2.0/N * (op("Sz", s1) * op("Sz", s2) +
                          1/2 * op("S+", s1) * op("S-", s2) +
                          1/2 * op("S-", s1) * op("S+", s2))
+
+            # make Trotter gate Gj that would correspond to each gate in the gate array of ITensors             
             Gj = exp(-im * tau/2 * hj)
+
+            # The push! function adds (appends) an element to the end of an array;
+            # ! performs an operation without creating a new object, (in a way overwites the previous array in consideration); 
+            # i.e. we append a new element Gj (which is an ITensor object representing a gate) to the end of the gates array.
             push!(gates, Gj)
         end
     end
 
+    # append! adds all the elements of a gates in reverse order (i.e. (N,N-1),(N-1,N-2),...) to the end of gates array.
     append!(gates, reverse(gates))
     return gates
 end
