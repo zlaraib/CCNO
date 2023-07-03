f9bf02269b8c502bc0ff7947f6c1acb11c662d6d
zlaraib <zlaraib@vols.utk.edu>
2023-07-03 01:38:24 -0400

    modelling Rog's vaccuum oscillation term

--- tests/time_evol.jl
+++ tests/time_evol.jl
@@ -1,64 +1,71 @@
 using ITensors
-
+using Plots 
+using Measures 
+using Tensorial
+using LinearAlgebra
 # We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
-# The technique used is "time evolving block decimation" (TEBD).
+# Here we are trying to simlate the vaccuum oscillatoin term in the Rogerro Hamiltonian 
 let
-  N = 100 # number of sites
-  cutoff = 1E-8 # specifies a truncation threshold for the SVD in MPS representation
-  tau = 0.1 # time step 
+  N = 4 # number of sites
+  cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
+  tau = 0.05 # time step 
   ttotal = 5.0 # total time of evolution 
 
   # Make an array of 'site' indices and label as s 
   # conserve_qns=true conserves the total spin quantum number "S"(in z direction) in the system as it evolves
-  s = siteinds("S=1/2", N; conserve_qns=false)  
+  s = siteinds("S=1/2", N; conserve_qns=true)  
 
   # Make gates (1,2),(2,3),(3,4),... i.e. unitary gates which act on neighboring pairs of sites in the chain.
   # Create an empty ITensors array that will be our Trotter gates
   # gates acts as a single-particle operator that does not actually entangle pairs
   gates = ITensor[] 
 
-  for j in 1:(N - 1)
-    #s1, s2 are neighbouring spin site/indices from the s array
-    s1 = s[j] 
-    s2 = s[j + 1]
+  for i in 1:(N-1)
+    for j in i+1:N
+      s1 = s[i]
+      s2 = s[j]
 
-    # Hamiltonian operator oriented in the x direction of neigbouring spin sites. 
-    # "Sx" and "Id" are inbuilt ITensor operators in ITensors library and op function applies these ITensors to a set of neigboring site indices aka pairs s1, s2.
-    # hj operator acts on the pair system such that h at site index j and j+1 is computed separately and then summed on this 1D chain.
-    # Identity operator on adjacent index is added to form a pair that attaches to the gate operator, hence gates is effectively a one-particle operator.
-    hj = pi * (
-      op("Sx", s1)* op("Id", s2)  + op("Sx", s2) * op("Id", s1)
-    )
+      # Hamiltonian operator oriented in the x direction of neigbouring spin sites. 
+      # "Sz" and "Id" are inbuilt ITensor operators in ITensors library and op function applies these ITensors to a set of neigboring site indices aka pairs s1, s2.
+      # hj operator acts on the pair system such that h at site index j and j+1 is computed separately and then summed on this 1D chain.
 
-    # make Trotter gate Gj that would correspond to each gate in the gate array of ITensors
-    Gj = exp(-im * tau / 2 * hj)  
+      hj = (
+        op("Sz", s1)* op("Id", s2) 
+        + op("Sz", s2)* op("Id", s1) 
+      )
 
-    # The push! function adds (appends) an element to the end of an array;
-    # ! performs an operation without creating a new object, (in a way overwites the previous array in consideration); 
-    # i.e. we append a new element Gj (which is an ITensor object representing a gate) to the end of the gates array.
-    push!(gates, Gj) 
+      # make Trotter gate Gj that would correspond to each gate in the gate array of ITensors
+      Gj = exp(-im * tau / 2 * hj)  
+
+      # The push! function adds (appends) an element to the end of an array;
+      # ! performs an operation without creating a new object, (in a way overwites the previous array in consideration); 
+      # i.e. we append a new element Gj (which is an ITensor object representing a gate) to the end of the gates array.
+      push!(gates, Gj) 
+    end
+
   end
 
   # append! adds all the elements of a gates in reverse order (i.e. (N,N-1),(N-1,N-2),...) to the end of gates array.
   append!(gates, reverse(gates)) 
 
   # Initialize psi to be a product state (alternating up and down)
-  psi = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
+  psi = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
 
   # center site because the Sz operator is arbitralily chosen to be placed at this site at each time step 
   c = div(N, 2) # c = N/2 
 
   # Create empty array to store sz values 
   Sz_array = Float64[] 
-
+  time = 0:tau:ttotal
   # Compute and print <Sz> at each time step then apply the gates to go to the next time
   for t in 0.0:tau:ttotal
     # compute initial expectation value of Sz(inbuilt operator in ITensors library) at the center of the chain (site c)
     sz = expect(psi, "Sz"; sites=c)
     # add an element sz to the end of Sz array  
     push!(Sz_array, sz) 
+
     println("$t $sz")
 
     # Writing an if statement in a shorthand way that checks whether the current value of t is equal to ttotal, 
@@ -73,18 +80,11 @@ let
     # The normalize! function is used to ensure that the MPS is properly normalized after each application of the time evolution gates. 
     # This is necessary to ensure that the MPS represents a valid quantum state.
     normalize!(psi) 
+
   end
 
-  # Checking that the value of Sz at the center spin oscillates between -0.5 and 0.5 every 5 timesteps
-  @assert Sz_array[1] == -0.5
-  @assert Sz_array[6] == 0.5
-  @assert Sz_array[11] == -0.5
-  @assert Sz_array[16] == 0.5
-  @assert Sz_array[21] == -0.5
-  @assert Sz_array[26] == 0.5
-  @assert Sz_array[31] == -0.5
-  @assert Sz_array[36] == 0.5
-  @assert Sz_array[41] == -0.5
-  @assert Sz_array[46] == 0.5
-  @assert Sz_array[51] == -0.5
+  #display(plot!(time[1;], Sz_array,xlabel = "t", ylabel = "Sz",margin=20mm))
+    # # Plotting Sz vs t
+    display(plot(0.0:tau:tau*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "Sz", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm)) 
+
 end
