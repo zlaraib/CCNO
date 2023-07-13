b03041e21ed7bb6e2814f0f25be459f57885d25e
zlaraib <zlaraib@vols.utk.edu>
2023-07-13 15:15:21 -0400

    changed syntax for power

--- tests/Rog_tests/src/constants.jl
+++ tests/Rog_tests/src/constants.jl
@@ -3,5 +3,5 @@ c = 2.99792458e10 # cm/s
 eV = 1.60218e-12 # erg
 MeV = 1e6 * eV
 GeV = 1e9 * eV
-GF = 1.1663787e-5 / GeV**2 * (hbar*c)**3 # erg cm^3
+GF = 1.1663787e-5 / GeV^2 * (hbar*c)^3 # erg cm^3
 kB = 1.3807e-16 # erg/K
