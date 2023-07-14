84192cbfca018785be6d70680fc9bdaf5e1e57ac
zlaraib <zlaraib@vols.utk.edu>
2023-07-14 01:32:47 -0400

    corrected syntax error for psi

--- tests/Rog_tests/src/expect.jl
+++ tests/Rog_tests/src/expect.jl
@@ -30,7 +30,7 @@ function evolve(s, tau, n, N, del_x, psi, cutoff, ttotal)
         # apply each gate in gates successively to the wavefunction psi (it is equivalent to time evolving psi according to the time-dependent Hamiltonian represented by gates).
         # The apply function is smart enough to determine which site indices each gate has, and then figure out where to apply it to our MPS. 
         # It automatically handles truncating the MPS and handles the non-nearest-neighbor gates in this example.
-     I  psi = apply(gates, psi; cutoff)
+	    psi = apply(gates, psi; cutoff)
         # The normalize! function is used to ensure that the MPS is properly normalized after each application of the time evolution gates. 
         # This is necessary to ensure that the MPS represents a valid quantum state.
         normalize!(psi)
