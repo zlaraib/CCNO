ba0084bc27615ff0cfd7d4c183277436076f90d4
zlaraib <zlaraib@vols.utk.edu>
2023-08-29 21:27:03 -0400

    vaccumm oscillations produced by B field in x and obey analytic results

--- tests/time_evol.jl
+++ tests/time_evol.jl
@@ -4,60 +4,62 @@ using Measures
 using LinearAlgebra
 include("Rog_tests/src/gates_function.jl")
 include("Rog_tests/src/constants.jl")
+
 # We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
-# Here we are trying to simlate the vaccuum oscillation term in the Rogerro Hamiltonian 
 let
-  N = 4 # number of sites
+  N = 2 # number of sites
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
-  tau = 0.05 # time step 
+  tau = 0.1 # time step 
   ttotal = 5.0 # total time of evolution 
   del_x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
-  del_m2= 1.93E-23 #ergs squared
-  tolerance  = 5E-1 # acceptable level of error or deviation from an exact value or solution
+  del_m2= 1.93E-23 # mass difference between the second and first neutrino mass eigenstates (associated with solar neutrino oscillations) in ergs^2
+  tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution
 
   # Make an array of 'site' indices and label as s 
   # conserve_qns=true conserves the total spin quantum number "S"(in z direction) in the system as it evolves
-  s = siteinds("S=1/2", N; conserve_qns=true)  
+  s = siteinds("S=1/2", N; conserve_qns=false)  
 
   # Initialize an array of ones for all N particles
   mu = zeros(N)
-    
+                                
   # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F)
   n = mu.* fill((del_x)^3/(sqrt(2) * G_F), N)
-    
-  # Create an array B with N elements. Each element of the array is a vector [0, 0, 1]
-  B = fill([0, 0, 1], N)
-    
+      
+  # Create an array B with N elements. Each element of the array is a vector [1, 0, 0]
+  B = fill([1, 0, 0], N)              
+      
   # Create an array of neutrino vaccum energy
   E = fill(4/(del_m2),N)
-
+  
   gates = create_gates(s, n, del_m2, B, E, N, del_x, tau)
-
+  
+  
   # Initialize psi to be a product state (alternating up and down)
-  psi = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
+  psi = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
 
+  # center site because the Sz operator is arbitralily chosen to be placed at this site at each time step 
+  c = div(N, 2) # c = N/2 
+  
   # Create empty array to store sz values 
   Sz_array = Float64[] 
-  prob_surv_array = Float64[]
-  time = 0:tau:ttotal
+
   # Compute and print <Sz> at each time step then apply the gates to go to the next time
   for t in 0.0:tau:ttotal
-    # compute initial expectation value of Sz(inbuilt operator in ITensors library) at the center of the chain (site c)
-    sz = expect(psi, "Sz"; sites=1)
+    # compute initial expectation value of Sz(inbuilt operator in ITensors library) at the first site of the chain
+    sz = expect(psi, "Sz"; sites=c)
     # add an element sz to the end of Sz array  
     push!(Sz_array, sz) 
+    println("$t $sz")
 
-    #println("$t $sz")
-    # survival probability for a (we took first) neutrino to be found in its initial flavor state (in this case a spin down)
-    prob_surv = 0.5 * (1 - 2 * sz)
-    # add an element prob_surv to the end of  prob_surv_array 
-    push!(prob_surv_array, prob_surv)
-    println("$t $prob_surv")
+    # Compute the expected value based on the derived analytic formula
+    expected_sz = 0.5 * cos(pi * t)
 
-    # Checking that the value of survival probbability at the 1st spin remains 1 at all times
-    @assert prob_surv == 1.0
+    # Checking that the value of Sz at the center spin oscillates between 0.5 and -0.5 
+    # Compare the actual value with the expected value using a tolerance
+    @assert abs(sz - expected_sz) < tolerance
+
     # Writing an if statement in a shorthand way that checks whether the current value of t is equal to ttotal, 
     # and if so, it executes the break statement, which causes the loop to terminate early.
     t ≈ ttotal && break  
@@ -70,12 +72,6 @@ let
     # The normalize! function is used to ensure that the MPS is properly normalized after each application of the time evolution gates. 
     # This is necessary to ensure that the MPS represents a valid quantum state.
     normalize!(psi) 
-    
-
   end
-
-    
-    # Plotting Survival probabilty vs t
-    display(plot(0.0:tau:tau*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "Sz", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm)) 
 
 end
