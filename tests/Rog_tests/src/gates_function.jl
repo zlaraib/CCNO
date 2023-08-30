b0ae12b3298f1395dca09dd3548d0cba5736742d
zlaraib <zlaraib@vols.utk.edu>
2023-08-29 21:24:03 -0400

    generalizing hj operator to include both vac osc and self-interaction terms using all to all i<j sites interactions

--- tests/Rog_tests/src/gates_function.jl
+++ tests/Rog_tests/src/gates_function.jl
@@ -1,23 +1,23 @@
 using ITensors
 include("constants.jl")
 
-# Expected units of the quantities used in gates
+# Expected units of the quantities used in gates                                                                    
 # del_m2 = ergs squared
 # hbar =  erg s
 # c = cm/s
-# G_F = erg cm^3
+# G_F = erg cm^3           
 # del_x = cm
 # E = erg
 
 function create_gates(s, n, del_m2, B, E, N, del_x, tau)
     # Make gates (1,2),(2,3),(3,4),... i.e. unitary gates which act on any (non-neighboring) pairs of sites in the chain.
     # Create an empty ITensors array that will be our Trotter gates
-    gates = ITensor[]
+    gates = ITensor[]                                                              
     
     for i in 1:(N-1)
         for j in i+1:N
             #s1, s2 are non-neighbouring spin site/indices from the s array
-            s1 = s[i]
+            s1 = s[i]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
             s2 = s[j]
             # assert B vector to have a magnitude of 1 while preserving its direction.
             @assert norm(B[i]) == 1
@@ -27,16 +27,16 @@ function create_gates(s, n, del_m2, B, E, N, del_x, tau)
             # op function returns these operators as ITensors and we tensor product and add them together to compute the operator hj.
             # ni and nj are the neutrions at site i and j respectively.
             # mu pairs divided by 2 to avoid double counting
-            hj = -((del_m2/(4*E[i])) * B[i][3] * (op("Sz", s1)* op("Id", s2) +  op("Sz", s2)* op("Id", s1))) +
-                    ((2.0* √2 * G_F * (n[i]+ n[j])/(2*((del_x)^3)) * 1/N) * 
-                    (op("Sz", s1) * op("Sz", s2) +
-                     1/2 * op("S+", s1) * op("S-", s2) +
-                     1/2 * op("S-", s1) * op("S+", s2)))
-
-            # writing in terms of mu 
-            # hj = (2.0* (mu[i]+ mu[j])/(2) * 1/N) * ((op("Sz", s1) * op("Sz", s2) +
-            # 1/2 * op("S+", s1) * op("S-", s2) +
-            # 1/2 * op("S-", s1) * op("S+", s2)))
+ 
+            hj = pi * (
+              op("Sx", s1)* op("Id", s2)  + op("Sx", s2) * op("Id", s1)
+            ) 
+            +
+            ((2.0* √2 * G_F * (n[i]+ n[j])/(2*((del_x)^3)) * 1/N) * 
+            (op("Sz", s1) * op("Sz", s2) +
+             1/2 * op("S+", s1) * op("S-", s2) +
+             1/2 * op("S-", s1) * op("S+", s2)))
+             
             # make Trotter gate Gj that would correspond to each gate in the gate array of ITensors             
             Gj = exp(-im * tau/2 * hj)
 
