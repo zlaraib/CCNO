5fa9e09044bc7be175322b8b24b0bcf70ff7b04d
zlaraib <zlaraib@vols.utk.edu>
2023-09-03 17:22:22 -0400

    minor changes in variables and comments

--- tests/Rog_tests/main_self_interaction.jl
+++ tests/Rog_tests/main_self_interaction.jl
@@ -11,10 +11,10 @@ include("src/constants.jl")
 function main()
     N = 4 # number of sites (NEED TO GO TILL 96 for Rog_results)
     cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT)
-    tau = 0.05 # time step (NEED TO BE 0.05 for Rog_results)
+    τ = 0.05 # time step (NEED TO BE 0.05 for Rog_results)
     ttotal = 5 # total time of evolution (NEED TO GO TILL 50 for Rog_results)
     tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution
-    del_x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
+    ∇x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
 
     # s is an array of spin 1/2 tensor indices (Index objects) which will be the site or physical indices of the MPS.
     # conserve_qns=true conserves the total spin quantum number "S" in the system as it evolves
@@ -29,7 +29,7 @@ function main()
     mu = ones(N) # erg
     
     # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F). This is the number of neutrinos 
-    n = mu .* fill((del_x)^3/(sqrt(2) * G_F), N)
+    n = mu .* fill((∇x)^3/(sqrt(2) * G_F), N)
     
     # Create an array B with N elements. Each element of the array is a vector [0, 0, 1]
     B = fill([0, 0, 1], N)
@@ -38,28 +38,29 @@ function main()
     ω = fill(0, N) 
 
     # Initialize psi to be a product state (alternating down and up)
-    psi = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
+    ψ = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
 
     #extract output from the expect.jl file where the survival probability values were computed at each timestep
-    Sz_array, prob_surv_array = evolve(s, tau, n, ω, B, N, del_x, psi, cutoff, ttotal)
+    Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
 
     #index of minimum of the prob_surv_array (containing survival probability values at each time step)
     i_min = argmin(prob_surv_array)
     # time at which the mimimum survival probability is reached
-    t_min = tau * i_min - tau
+    t_min = τ * i_min - τ
     # Rogerro(2021)'s fit for the first minimum of the survival probability reached for a time t_p 
     t_p_Rog = a_t*log(N) + b_t * sqrt(N) + c_t
     println("t_p_Rog= ",t_p_Rog)
     println("i_min= ", i_min)
     println("t_min= ", t_min)
     # Check that our time of minimum survival probability compared to Rogerro(2021) remains within the timestep and tolerance.
-    @assert abs(t_min - t_p_Rog) <  tau + tolerance 
+    @assert abs(t_min - t_p_Rog) <  τ + tolerance 
 
     # Plotting P_surv vs t
-    plot(0.0:tau:tau*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
+    plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
 
     # Save the plot as a PDF file
     savefig("plot.pdf")
 end 
 
 @time main()
+
--- tests/time_evol.jl
+++ tests/time_evol.jl
@@ -11,20 +11,20 @@ include("Rog_tests/src/constants.jl")
 function main()
   N = 2 # number of sites
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
-  tau = 0.1 # time step 
+  τ = 0.1 # time step 
   ttotal = 5.0 # total time of evolution 
-  del_x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
+  ∇x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
   tolerance  = 1E-5 # acceptable level of error or deviation from the exact value or solution
 
   # Make an array of 'site' indices and label as s 
   # conserve_qns=true conserves the total spin quantum number "S"(in z direction) in the system as it evolves
   s = siteinds("S=1/2", N; conserve_qns=false)  
 
-  # Initialize an array of ones for all N particles
+  # Initialize an array of zeros for all N particles
   mu = zeros(N)
                                 
   # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F)
-  n = mu.* fill((del_x)^3/(sqrt(2) * G_F), N)
+  n = mu.* fill((∇x)^3/(sqrt(2) * G_F), N)
       
   # Create an array B with N elements. Each element of the array is a vector [1, 0, 0]
   B = fill([1, 0, 0], N)              
@@ -32,18 +32,20 @@ function main()
   # Create an array ω with N elements. Each element of the array is a const pi.
   ω = fill(π, N) 
 
-  gates = create_gates(s, n, ω, B, N, del_x, tau)
+  gates = create_gates(s, n, ω, B, N, ∇x, τ)
   
   # Initialize psi to be a product state (alternating up and down)
-  psi = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
+  ψ = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
 
   #extract output from the expect.jl file where the survival probability values were computed at each timestep
-  Sz_array, prob_surv_array = evolve(s, tau, n, ω, B, N, del_x, psi, cutoff, tolerance, ttotal)
+  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
   # Plotting P_surv vs t
-  plot(0.0:tau:tau*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
+  plot(0.0:τ:τ*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
 
   # Save the plot as a PDF file
   savefig("plot_1.pdf")
 end
 
 @time main()
+
+
