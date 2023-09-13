8d71f8da8afff156b69b2dfc7c08b9d2a3471fdd
zlaraib <zlaraib@vols.utk.edu>
2023-09-13 03:11:23 -0400

    symbol nabla --> Delta and added if else statemnet to print correct arrays for all scripts

--- src/evolution.jl
+++ src/evolution.jl
@@ -1,7 +1,7 @@
 include("gates_function.jl")  # Include the gates_functions.jl file
 
 
-function evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
+function evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, tolerance, ttotal)
     
     # Create empty array to store sz values 
     Sz_array = Float64[]
@@ -9,7 +9,7 @@ function evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
     prob_surv_array = Float64[]
 
     # extract the gates array generated in the gates_function file
-    gates = create_gates(s, n, ω, B, N, ∇x, τ)
+    gates = create_gates(s, n, ω, B, N, Δx, τ)
 
     # Compute and print survival probability (found from <Sz>) at each time step then apply the gates to go to the next time
     for t in 0.0:τ:ttotal
@@ -19,12 +19,13 @@ function evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
         # add an element sz to the end of Sz array 
         push!(Sz_array, sz)
         
-        println("$t $sz")
+        
         # survival probability for a (we took first) neutrino to be found in its initial flavor state (in this case a spin down)
         prob_surv = 0.5 * (1 - 2 * sz)
         # add an element prob_surv to the end of  prob_surv_array 
         push!(prob_surv_array, prob_surv)
-            
+        
+        
         for i in 1:(N-1)
             if ω[i] != 0
                 # Compute the expected value based on the derived analytic formula
@@ -33,11 +34,14 @@ function evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
                 # Checking that the value of Sz at the center spin oscillates between 0.5 and -0.5 
                 # Compare the actual value with the expected value using a tolerance
                 @assert abs(sz - expected_sz) < tolerance
-            else println("$t $prob_surv")
             end
-        
-
         end
+
+        if ω == fill(0, N) 
+            println("$t $prob_surv")
+        else println("$t $sz")
+        end
+
         # Writing an if statement in a shorthand way that checks whether the current value of t is equal to ttotal, 
         # and if so, it executes the break statement, which causes the loop to terminate early.
         t ≈ ttotal && break
