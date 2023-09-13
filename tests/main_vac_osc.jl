86dd8c8c2091517b34b5fbe825201d0a11a87058
zlaraib <zlaraib@vols.utk.edu>
2023-09-13 03:08:06 -0400

    symbol nabla --> Delta

--- tests/main_vac_osc.jl
+++ tests/main_vac_osc.jl
@@ -13,7 +13,7 @@ function main()
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
   τ = 0.1 # time step 
   ttotal = 5.0 # total time of evolution 
-  ∇x = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
+  Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
   tolerance  = 1E-5 # acceptable level of error or deviation from the exact value or solution
 
   # Make an array of 'site' indices and label as s 
@@ -24,7 +24,7 @@ function main()
   mu = zeros(N)
                                 
   # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F)
-  n = mu.* fill((∇x)^3/(sqrt(2) * G_F), N)
+  n = mu.* fill((Δx)^3/(sqrt(2) * G_F), N)
       
   # Create a B vector which would be same for all N particles 
   B = [1, 0, 0]           
@@ -32,13 +32,13 @@ function main()
   # Create an array ω with N elements. Each element of the array is a const pi.
   ω = fill(π, N) 
 
-  gates = create_gates(s, n, ω, B, N, ∇x, τ)
+  gates = create_gates(s, n, ω, B, N, Δx, τ)
   
   # Initialize psi to be a product state (alternating up and down)
   ψ = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
 
   #extract output from the expect.jl file where the survival probability values were computed at each timestep
-  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, ∇x, ψ, cutoff, tolerance, ttotal)
+  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, tolerance, ttotal)
   # Plotting P_surv vs t
   plot(0.0:τ:τ*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", legend = false, size=(700, 600), aspect_ratio=:auto,margin= 10mm) 
 
