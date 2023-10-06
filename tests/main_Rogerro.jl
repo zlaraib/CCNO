164ce8b16fe28892fa50e47be5e3d044b750ef15
zlaraib <zlaraib@vols.utk.edu>
2023-10-05 23:00:19 -0400

    changed B as in Rog, added delta_w definition and changed first local minima assert condition

--- tests/main_Rogerro.jl
+++ tests/main_Rogerro.jl
@@ -9,10 +9,10 @@ include("../src/constants.jl")
 # which is a product state where each site alternates between up and down.
 
 function main()
-    N = 6 # number of sites (NEED TO GO TILL 96 for Rog_results)
+    N = 4 # number of sites (NEED TO GO TILL 96 for Rog_results)
     cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT)
     τ = 0.05 # time step (NEED TO BE 0.05 for Rog_results)
-    ttotal = 5 # total time of evolution (NEED TO GO TILL 50 for Rog_results)
+    ttotal = 3 # total time of evolution (NEED TO GO TILL 50 for Rog_results)
     tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution
     Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
 
@@ -33,39 +33,61 @@ function main()
     n = mu .* fill((Δx)^3/(sqrt(2) * G_F), N)
     
     # Create a B vector which would be same for all N particles 
-    B = [0, 0, 1]
+    B = [0, 0, -1]
 
     # Create arrays ω_a and ω_b
-    ω_a = 0.5*fill(2, div(N, 2))
-    ω_b = 0.5*fill(0, div(N, 2))
+    ω_a = fill(2, div(N, 2))
+    ω_b = fill(0, div(N, 2))
 
+    # Defining Δω as in Rogerro(2021)
+    Δω = (ω_a - ω_b)/2
+    
     # Concatenate ω_a and ω_b to form ω
     ω = vcat(ω_a, ω_b)
 
-    # Initialize psi to be a product state (First half to be spin down and other half to be spin up)
-    #ψ = productMPS(s, n -> isodd(n) ? "Dn" : "Up")
-
     ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up")
-    #ψ = productMPS(s, n -> n == 1 || n == 2 || n == 3 ? "↓" : "↑")
 
     #extract output from the expect.jl file where the survival probability values were computed at each timestep
     Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, ttotal)
 
-    #index of minimum of the prob_surv_array (containing survival probability values at each time step)
-    i_min = argmin(prob_surv_array)
-    # time at which the mimimum survival probability is reached
-    t_min = τ * i_min - τ
+    # This function scans through the array, compares each element with its neighbors, 
+    # and returns the index of the first local minimum it encounters. 
+    # If no local minimum is found, it returns -1 to indicate that.
+    function find_first_local_minima_index(arr)
+        n = length(arr)
+        for i in 2:(n-1)
+            if arr[i] < arr[i-1] && arr[i] < arr[i+1]
+                return i
+            end
+        end
+        return -1  
+    end
+    
+    # Index of first minimum of the prob_surv_array (containing survival probability values at each time step)
+    i_first_local_min = find_first_local_minima_index(prob_surv_array)
+    
+    # Writing if_else statement to communicate if local minima (not) found
+    if i_first_local_min != -1
+        println("Index of the first local minimum: ", i_first_local_min)
+    else
+        println("No local minimum found in the array.")
+    end
+
+    # Time at which the first mimimum survival probability is reached
+    t_min = τ * i_first_local_min - τ
+    println("Corresponding time of first minimum index= ", t_min)
+
     # Rogerro(2021)'s fit for the first minimum of the survival probability reached for a time t_p 
     t_p_Rog = a_t*log(N) + b_t * sqrt(N) + c_t
     println("t_p_Rog= ",t_p_Rog)
-    println("i_min= ", i_min)
-    println("t_min= ", t_min)
-    # Check that our time of minimum survival probability compared to Rogerro(2021) remains within the timestep and tolerance.
+
+    # Check that our time of first minimum survival probability compared to Rogerro(2021) remains within the timestep and tolerance.
     #@assert abs(t_min - t_p_Rog) <  τ + tolerance 
 
     # Plotting P_surv vs t
-    plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
-
+    plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)",title = "Running main_Rogerro script", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm, label= ["My_plot_for_N$(N)"]) 
+    scatter!([t_p_Rog],[prob_surv_array[i_first_local_min]], label= ["t_p_Rog"])
+    scatter!([t_min],[prob_surv_array[i_first_local_min]], label= ["My_t_min)"], legendfontsize=5, legend=:topright)
     # Save the plot as a PDF file
     savefig("Survival probability vs t (Rog).pdf")
 end 
