85e4ab14eec161183a72cb9bdd963c2bb4d5ba46
zlaraib <zlaraib@vols.utk.edu>
2023-10-19 03:16:04 -0400

    made minor changes to keep pipeline consistent

--- tests/main_vac_osc.jl
+++ tests/main_vac_osc.jl
@@ -5,9 +5,12 @@ using LinearAlgebra
 include("../src/evolution.jl")
 include("../src/constants.jl")
 
+#This test needs to produce consistent results when we write ω in terms of norm(p). That part is still not done. 
+
 # We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
+
 function main()
   N = 4 # number of sites
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
@@ -17,7 +20,7 @@ function main()
   tolerance  = 1E-5 # acceptable level of error or deviation from the exact value or solution
 
   # Make an array of 'site' indices and label as s 
-  # conserve_qns=true conserves the total spin quantum number "S"(in z direction) in the system as it evolves
+  # conserve_qns=false doesnt conserve the total spin quantum number "S"(in z direction) in the system as it evolves
   s = siteinds("S=1/2", N; conserve_qns=false)  
 
   # Initialize an array of zeros for all N particles
@@ -27,25 +30,67 @@ function main()
   n = mu.* fill((Δx)^3/(sqrt(2) * G_F), N)
       
   # Create a B vector which would be same for all N particles 
-  B = [1, 0, 0]           
+  B = [1, 0, 0]          
   
   # Create an array ω with N elements. Each element of the array is a const pi.
-  ω = fill(π, N) 
+  ω = fill(π, N)
 
-  gates = create_gates(s, n, ω, B, N, Δx, τ)
+  ###Any and all possible values work for now b/c they are not being tested in the vacuum oscillation part of hamiltonian/gates
+  x = zeros(N)
+  y = fill(rand(), N)
+  z = zeros(N)
+  p = zeros(N, 3) 
+  Δp = 1
+  ###
+
+  gates = create_gates(s, n, ω, B, N, Δx, p, x, Δp, τ)
   
-  # Initialize psi to be a product state (alternating up and down)
-  ψ = productMPS(s, n -> isodd(n) ? "Up" : "Dn")
+  # Initialize psi to be a product state (First half to be spin down and other half to be spin up)
+  ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up")
 
   #extract output from the expect.jl file where the survival probability values were computed at each timestep
-  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, tolerance, ttotal)
-  # Plotting P_surv vs t
-  plot(0.0:τ:τ*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", legend = false, size=(700, 600), aspect_ratio=:auto,margin= 10mm) 
+  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, p, x, Δp, ψ, cutoff, tolerance, ttotal)
+  
+  expected_sz_array = Float64[]
+  expected_sz= Float64[]
+  
+  for t in 0.0:τ:ttotal
 
+    for i in 1:(N-1)
+
+        if ω[i] != 0
+
+          if B[1] == 1
+
+            # Compute the expected value based on the derived analytic formula
+            expected_sz = -0.5 * cos(ω[i] * t)
+
+          end
+          if B[3] == -1
+
+            # Compute the expected value based on the derived analytic formula
+            expected_sz = -0.5
+
+          end
+
+        end
+
+    end
+
+    push!(expected_sz_array, expected_sz)
+
+  end
+
+  # Check if every element in Sz_array is less than tolerance away from the corresponding element in expected_sz_array
+  # for B vector in x, it checks that the value of Sz at the first spin site oscillates between -0.5 and 0.5 
+  # for B vector in -z, it checks that the value of Sz at the firstspin site never oscillates from -0.5 
+  @assert all(abs.(Sz_array .- expected_sz_array) .< tolerance)
+
+  # Plotting P_surv vs t
+  plot(0.0:τ:τ*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", title = "Running main_vac_osc script",legend = true, size=(700, 600), aspect_ratio=:auto,margin= 10mm, label = "My_sz") 
+  plot!(0.0:τ:τ*(length(Sz_array)-1), expected_sz_array, xlabel = "t", ylabel = "<Sz>", title = "Running main_vac_osc script", legendfontsize=8, legend=:topright, label = "Expected_sz from Sakurai") 
   # Save the plot as a PDF file
   savefig("<Sz> vs t (only vacuum oscillation term plot).pdf")
 end
 
 @time main()
-
-
