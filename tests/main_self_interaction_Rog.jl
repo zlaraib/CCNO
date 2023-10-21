e4f95cac382dcc458e30366561136bf1ceaba2d5
zlaraib <zlaraib@vols.utk.edu>
2023-10-21 04:46:37 -0400

    making the test file more readable and using more physical parameters

--- tests/main_self_interaction_Rog.jl
+++ tests/main_self_interaction_Rog.jl
@@ -4,67 +4,61 @@ using Measures
 using LinearAlgebra
 include("../src/evolution.jl")
 include("../src/constants.jl")
-
-# Discuss the corrrect conditions to put for implementing self interactions as done in rogerros paper and make those changes. 
-
+include("../src/shape_func.jl")
+include("../src/momentum.jl")
 
 # We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
 
 function main()
-    N = 4 # number of sites (NEED TO GO TILL 96 for Rog_results)
-    cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT)
-    τ = 0.05 # time step (NEED TO BE 0.05 for Rog_results)
-    ttotal = 5 # total time of evolution (NEED TO GO TILL 50 for Rog_results)
-    tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution
-    Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
+    N = 4 # number of sites (NEED TO GO TILL 96 for Rog_results) # variable.
+    cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT) # variable.
+    τ = 0.05 # time step #fixed for Rogerros result
+    ttotal = 5 # total time of evolution (NEED TO GO TILL 50 for Rog_results) # variable.
+    tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution # variable.
+    Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm  # variable.
+    Δp = rand() # shape function width # variable.
+    del_m2 = 0.0 # Fixed for rog test case. Please dont play with it. 
+
 
     # s is an array of spin 1/2 tensor indices (Index objects) which will be the site or physical indices of the MPS.
     # We overload siteinds function, which generates custom Index array with Index objects having the tag of total spin quantum number for all N.
     # conserve_qns=true conserves the total spin quantum number "S" in the system as it evolves
-    s = siteinds("S=1/2", N; conserve_qns=true)  
+    s = siteinds("S=1/2", N; conserve_qns=true)  #fixed
 
-    # Constants for Rogerro's fit (only self-interaction term)
+    # Fixed Constants for Rogerro's fit (only self-interaction term)
     a_t = 0
     b_t = 2.105
     c_t = 0
     
     # Initialize an array of ones for all N sites
-    mu = ones(N) # erg
+    mu = ones(N) # erg #fixed
     
     # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F). This is the number of neutrinos. 
     n = mu .* fill((Δx)^3/(sqrt(2) * G_F), N)
     
     # Create a B vector which would be same for all N particles 
-    B = [0, 0, 1]
+    B = [0, 0, -1] # fixed for Rogerro's case
 
-    # Create an array ω with N elements. Each element of the array is zero.
-    ω = fill(0, N) 
-    x = fill(rand(), N)
-    y = fill(rand(), N)
-    z = fill(rand(), N)
-    ###
-    # For now, using Rog results to have these conditions
-    # array p with N rows and 3 columns, all initialized to 0.0 with colums representing components and rows representing sites
-    p = zeros(N, 3) 
-    Δp = Inf
-    ###
-
-    println(p) 
-    unit_p = p/norm(p)
-    println(norm(p))
-    println(unit_p) 
-    del_m2 = 2.0
-    #norm(p) = [del_m2 / (2 * ω[i]) for i in 1:N]
-    # The norm function returns the magnitude of the vector (by default):
-    # A unit vector is then found by scaling by the reciprocal of the magnitude: e.g array/vector v  can have unit vector v to be  defined as: v / norm(v)
+    x = fill(rand(), N) # variable.
+    y = fill(rand(), N) # variable.
+    z = fill(rand(), N) # variable.
 
     # Initialize psi to be a product state (First half to be spin down and other half to be spin up)
-    ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up")
+    ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up") #fixed for Rog case
 
-    #extract output from the expect.jl file where the survival probability values were computed at each timestep
-    Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, p, x, Δp, ψ, cutoff, tolerance, ttotal)
+    #Select a shape function based on the shape_name variable form the list defined in dictionary in shape_func file
+    shape_name = "none"  # Change this to the desired shape name # variable.
+
+    # array p with N rows and 3 columns, all initialized to 0.0 with colums representing components and rows representing sites
+    p = zeros(N, 3) #fixed for Rogerro's case
+
+    # extract output of p_hat and p_mod for the p vector defined above for all sites. 
+    p_mod, p_hat = momentum(p,N)
+
+    #extract output for the survival probability values at each timestep
+    Sz_array, prob_surv_array = evolve(s, τ, n, B, N, Δx,del_m2, p, p_mod, p_hat, x, Δp, ψ, shape_name, cutoff, tolerance, ttotal)
 
     # This function scans through the array, compares each element with its neighbors, 
     # and returns the index of the first local minimum it encounters. 
@@ -101,7 +95,7 @@ function main()
     @assert abs(t_min - t_p_Rog) <  τ + tolerance 
 
     # Plotting P_surv vs t
-    plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)",title = "Running main_self_interaction script", legend = true, size=(800, 600), aspect_ratio=:auto,margin= 10mm, label= ["My_plot_for_N$(N)"]) 
+    plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)",title = "Running main_self_interaction_Rog script", legend = true, size=(800, 600), aspect_ratio=:auto,margin= 10mm, label= ["My_plot_for_N$(N)"]) 
     scatter!([t_p_Rog],[prob_surv_array[i_first_local_min]], label= ["t_p_Rog"])
     scatter!([t_min],[prob_surv_array[i_first_local_min]], label= ["My_t_min)"], legendfontsize=5, legend=:topright)
     # Save the plot as a PDF file
