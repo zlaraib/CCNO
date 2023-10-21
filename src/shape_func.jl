75e436a4a4b1fc7f85028a2c0dc32420d7fd710f
zlaraib <zlaraib@vols.utk.edu>
2023-10-21 04:41:16 -0400

    removed for loops on sites and passed the sites as arguments infunction signature + extended shape func dictionary to include shape and no shape + took shape function width outside of the source code file

--- src/shape_func.jl
+++ src/shape_func.jl
@@ -1,5 +1,9 @@
 include("heaviside.jl")
-# Δp  = rand()  # width of the shape function # move it to the tests script outside
+
+""" 
+    This file introduces the different shapes for neutrinos and map those shapes to
+    a dictionary to allow easy switching between shapes in test files. 
+"""
 
 function flat_top(Δp, ξ)
     return 1 / Δp * heaviside(1/2 - abs(ξ))
@@ -9,29 +13,26 @@ function triangular(Δp, ξ)
     return 1 / Δp * (1 - abs(ξ)) * heaviside(1 - abs(ξ))
 end
 
-function shape_func(x, Δp, N)
-    if Δp == Inf
-        return 1  # using Δp infinite as Rog case for now 
+function none(Δp, ξ)
+    return 1 
+end
+
+function shape_func(x, Δp, i, j, shape_name)
+
+    # Define a dictionary mapping shape names to functions
+    shapes = Dict(
+        "flat_top" => flat_top,
+        "triangular" => triangular,
+        "none" => none
+    )
+    
+    if haskey(shapes, shape_name)
+        shape_function = shapes[shape_name]  # assign the corresponding function (selected in shape_name in the test file) to shape_function.
+        # ξ determines the overlap of neutrinos (i.e. their shapes) on top of each other to signify the neutrino-neutrino interaction strength.
+        ξ = (x[i] - x[j]) / Δp # dependent upon difference in distance of i and j sites and the width of the shape function. 
+        return shape_function(Δp, ξ)
     else
-        # Define a dictionary mapping shape names to functions
-        shapes = Dict(
-            "flat_top" => flat_top,
-            "triangular" => triangular
-        )
-        #Select a shape function based on the shape_name variable form the list defined in dictionary 
-        shape_name = "flat_top"  # Change this to the desired shape name
-        if haskey(shapes, shape_name)
-            shape_function = shapes[shape_name] # assign the corresponding function (selected in shape_name above) to shape_function.
-            result = zeros(N)  # Adjust the dimensions as needed rn just initialized to N 
-            for i in 1:(N - 1)
-                for j in (i+1):N
-                    ξ = (x[i] - x[j]) / Δp
-                    result = shape_function(Δp, ξ)
-                end
-            end
-            return result
-        else
-            error("Unknown shape: $shape_name")
-        end
+        error("Unknown shape: $shape_name")
     end
+
 end
