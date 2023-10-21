82bc466661aa947bc780d33573fd7e9e4811fe4e
zlaraib <zlaraib@vols.utk.edu>
2023-10-21 04:34:39 -0400

    removed for loop on sites and added sites dependence in the function argument

--- src/geometric_func.jl
+++ src/geometric_func.jl
@@ -1,14 +1,15 @@
-function geometric_func(N,p)
 
-    for i in 1:(N-1)
-        for j in (i+1):N
-            if dot(p[i, :], p[j, :]) ≈ 0.0
-                return 1
-            else
-                return 1 - dot(p[i, :], p[j, :])
-            end
-        end
+""" 
+    This function takes in a momentum vector (for all sites) along with their directions 
+    and returns the geometric factor dependent on the unit momentum vectors for all
+    i< j sites for a (non) zero p vector. 
+"""
+function geometric_func(p, p_hat, i, j)
+
+    if p[i] == 0  && p[j] == 0.0 
+        return 1
+    else
+        return 1 - dot(p_hat[i, :], p_hat[j, :])
     end
 
-    return 0  # Add a default return value if no perpendicular pairs are found
 end
