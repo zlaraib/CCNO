268a70f54dae1133361b741e2fff630b318e896f
zlaraib <zlaraib@vols.utk.edu>
2023-11-17 04:20:36 -0500

    added Richers I.C's in PIC treatment

--- tests/main_self_interaction.jl
+++ tests/main_self_interaction.jl
@@ -7,65 +7,79 @@ include("../src/constants.jl")
 include("../src/shape_func.jl")
 include("../src/momentum.jl")
 
-# BTW this test is not finding the first minima(as was proposed in Rog paper), just the global minima. Discuss optimal conditions.
-# So, need to put appropriate assert conditions.
-
 # BTW When you apply gates to an MPS, it will in general increase the bond dimension 
 # (one exception is that single-site gates don’t change the bond dimension), 
 # and if you continue applying gates without truncation the bond dimension will in general grow exponentially.
 # There isn’t a way to set the truncation when you initialize the state
 # this is all from julia itensors discourse answered by matthew fisherman
+
+
 # We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
+# throughout this code the words particle and sites are used interchangeably.
+# Where each site is occupied by either some neutrinos or some antineutrinos. 
 
 function main()
-    N_sites = 4 # number of sites # variable
-    cutoff = 1E-8 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT) #variable
-    τ = 0.05 # time step # variable
-    ttotal = 150 # total time of evolution #variable
+    N_sites = 8 # number of sites # variable
+    cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT) #variable
+    τ = 0.05 # time step # sec # variable
+    ttotal = 20 # total time of evolution # sec #variable
     tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution #variable
-    Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm  #variable
-    Δp = 5 # width of shape function #variable
-    del_m2 = 0 # fixed for 'only' self interactions
-    maxdim = 2 # max bond dimension in MPS truncation
+    Δx = 1E-3 # length of the box of interacting neutrinos at a site in cm  #variable
+    Δp = 5 # width of shape function  # cm #variable
+    del_m2 = 0 # fixed for 'only' self interactions # (erg^2)
+    maxdim = 500 # max bond dimension in MPS truncation
 
     #Select a shape function based on the shape_name variable form the list defined in dictionary in shape_func file
     shape_name = "triangular"  # Change this to the desired shape name #variable 
 
-    # # n = sum N_i /V # total number density
-    # # N = n * V 
-    # # n_i = N_/N_sites
+    """
+    PIC SETUP: 
 
-    # # Sherwood's initial conditions: # throughout this cdoe i am assuming each site is occupied by a particlei.e. each site containsa umber of neutrinos of all sma eflvor 
-    # # so all neutrinos are electron flavored while all muon flavored are anti neutrinos
+    big box for all 'sites' or particles 
+    total vol = V 
+    length of each side = L 
+    V = L^3 
+    N_sites = total no.of sites or particles
+    N =  total no.of neutrinos at all sites
+    n = total no.density of neutrinos at all sites
+    n = N / V 
+
+    small box (for each 'site')
+    total vol = V_i 
+    length of each side = Δx 
+    Δx^3 = V_i
+    N_i =  total no.of neutrinos at site i 
+    n_i  = total no.density of neutrinos at site i 
+    N_i = n_i  * V_i  
+
+    Combining 
+    sum n_i = n # total no.density of neutrinos
+    sum N_i = N # total no.of neutrinos
+    sum V_i = V # total volume of the grid
+    sum Δx_i = L # domain size
+
+    """
+
+    # Richers(2021) initial conditions:
+    # throughout this code I am assuming each site is occupied by a particle i.e. each site contains some number of neutrinos all of same flavor 
+    # so all neutrinos are electron flavored (at a site) which interact with electron flavored anti neutrinos (at a different site) in the opposing beam.
     L = 1 # cm # domain size
-    # n_mu_e =  4.891290848285061e+32 # cm^-3 # number density of electron flavor of neutrino
-    # N_sites= 50 # total sites/ particles # particles evenly spaced for each flavor 
+    n_mu_e =  4.891290848285061e+32 # cm^-3 # number density of electron flavor neutrino
+    n_mu_e_bar =  4.891290848285061e+32 # cm^-3 # number density of electron flavor antineutrino
+    # N_sites= 50 # total sites/particles that evenly spaced "for each (electron) flavor" 
+    #N_sites = 100 # total particles/sites for all neutrino and anti neutrino electron flavored
 
-
-    # # big box for all 'sites'/particles
-    # # total vol = V 
-    # # length of each side = L 
-    # V = L^3 
-    # # N =  total no.of 'particles'
-    # # n = total no.density of 'particles'
-    # n = N / V
-
-    # # small box (for each 'site')
-    # # total vol = V_i 
-    # # length of each side = Δx 
-    #  Δx^3 = V_i
-    # # N_i =  total no.of 'particles' at site i 
-    # # n_i  = total no.density of 'particles' at site i 
-    # N_i = n_i  * V_i  
-
-    # # Combining 
-    # # sum n_i = n # total no.density 
-    # # sum N_i = N # total no.particles
-    # # sum V_i = V # total volume 
-    # # sum Δx_i = L # domain size
-
+    V = L^3 
+    N_mu_e  = n_mu_e * V 
+    N_1 = fill(N_mu_e / (N_sites ÷ 2), N_sites ÷ 2)
+    N_mu_e_bar  = n_mu_e_bar * V 
+    N_2 = fill(N_mu_e_bar / (N_sites ÷ 2), N_sites ÷ 2)
+    # Create an array of dimension N and fill it half with values of sites containing all electron neutrinos 
+    # and other half with sites conatining electron anti-neutrino. 
+    # This is the total number of neutrinos. 
+    N = vcat(N_1, N_2)
 
     # s is an array of spin 1/2 tensor indices (Index objects) which will be the site or physical indices of the MPS.
     # We overload siteinds function, which generates custom Index array with Index objects having the tag of total spin quantum number for all N.
@@ -73,13 +87,6 @@ function main()
     # i.e. examples of conservation of quantum numbers are the total number of neutrino particles, or the total of all S_z components of this system of spins
     s = siteinds("S=1/2", N_sites; conserve_qns=true) #fixed
     
-    # Initialize an array of ones for all N sites
-    mu = ones(N_sites) # erg #variable
-    
-    # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F). This is the total number of neutrinos. 
-    N = mu .* fill((L)^3/(sqrt(2) * G_F), N_sites) #fixed
-        
-    n = N/((L)^3) #number density of all neutrinos
     # Create a B vector which would be same for all N particles 
     B = [0, 0, -1] #variable
 
@@ -105,7 +112,7 @@ function main()
     ψ = productMPS(s, N -> N <= N_sites/2 ? "Up" : "Dn") # Fixed to produce consistent results for the test assert conditions 
 
     #extract output for the survival probability values at each timestep
-    Sz_array, prob_surv_array = evolve(s, τ, N, B, N_sites, Δx, del_m2, p, x, Δp, ψ, shape_name, energy_sign, cutoff, tolerance, ttotal)
+    Sz_array, prob_surv_array = evolve(s, τ, N, B, N_sites, Δx,del_m2, p, x, Δp, ψ, shape_name, energy_sign, cutoff, maxdim, tolerance, ttotal)
 
     # Plotting P_surv vs t
     plot(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probabillity p(t)", legend = false, size=(800, 600), aspect_ratio=:auto,margin= 10mm) 
