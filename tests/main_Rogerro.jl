6812920c46ec16a5afa1374767ff515419003297
zlaraib <zlaraib@vols.utk.edu>
2024-01-24 22:18:37 -0500

    experimenting tdvp

--- tests/main_Rogerro.jl
+++ tests/main_Rogerro.jl
@@ -1,7 +1,8 @@
 using ITensors
 using Plots
 using Measures
-using ITensorTDVP
+# using ITensorTDVP
+using TimeEvoMPS
 include("../src/evolution.jl")
 include("../src/constants.jl")
 
@@ -16,7 +17,8 @@ function main()
     ttotal = 20 # total time of evolution (NEED TO GO TILL 50 for Rog_results)
     tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution
     Δx = 1E-3 # length of the box of interacting neutrinos at a site/shape function width of neutrinos in cm 
-
+    # maxdim = 10000
+    cutoff = 1E-14
     # s is an array of spin 1/2 tensor indices (Index objects) which will be the site or physical indices of the MPS.
     # We overload siteinds function, which generates custom Index array with Index objects having the tag of total spin quantum number for all N.
     # conserve_qns=false doesnt conserve the total spin quantum number "S" in the system as it evolves
