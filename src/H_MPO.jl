c38b305fd195af20c6590b341f883f31cdcb0f75
zlaraib <zlaraib@vols.utk.edu>
2024-02-05 17:16:18 -0500

    renamed N to N_sites

--- src/H_MPO.jl
+++ src/H_MPO.jl
@@ -1,12 +1,12 @@
 
 include("constants.jl")
 
-function Hamiltonian_mpo(s,n, ω, B, N, Δx,energy_sign)
+function Hamiltonian_mpo(s,n, ω, B, N_sites, Δx,energy_sign)
     #input the operator terms
     os = OpSum()                                                            
 
-    for i in 1:(N-1)
-        for j in i+1:N
+    for i in 1:(N_sites-1)
+        for j in i+1:N_sites
             # assert B vector to have a magnitude of 1 while preserving its direction.
             @assert norm(B) == 1
 
@@ -15,12 +15,12 @@ function Hamiltonian_mpo(s,n, ω, B, N, Δx,energy_sign)
             # mu pairs divided by 2 to avoid double counting
              
             if energy_sign[i]*energy_sign[j]>0
-                interaction_strength = (2.0/N * √2 * G_F * (n[i]+ n[j])/(2*((Δx)^3))) 
+                interaction_strength = (2.0/N_sites * √2 * G_F * (n[i]+ n[j])/(2*((Δx)^3))) 
                 os+= interaction_strength,"Sz",i,"Sz", j 
                 os+= 1/2,interaction_strength,"S+",i,"S-",j
                 os+=  1/2,interaction_strength,"S-",i,"S+",j
             else
-                interaction_strength = (2.0/N * √2 * G_F * (n[i]+ n[j])/(2*((Δx)^3))) 
+                interaction_strength = (2.0/N_sites * √2 * G_F * (n[i]+ n[j])/(2*((Δx)^3))) 
                 os+= 2,interaction_strength ,"Sz",i,"Sz", j 
                 os+= -interaction_strength ,"S+",i,"S-",j
                 os+=  -interaction_strength ,"S-",i,"S+",j
@@ -28,7 +28,7 @@ function Hamiltonian_mpo(s,n, ω, B, N, Δx,energy_sign)
 
             if ω[i] != 0 && ω[j] != 0
                 if energy_sign[i]*energy_sign[j]>0
-                    numerical_factor = (1/(N-1))
+                    numerical_factor = (1/(N_sites-1))
                     os+= numerical_factor,ω[i],B[1],"Sx",i,"Id",j  
                     os+= numerical_factor,ω[i],B[2],"Sy",i,"Id",j 
                     os+= numerical_factor,ω[i],B[3],"Sz",i,"Id",j 
@@ -36,7 +36,7 @@ function Hamiltonian_mpo(s,n, ω, B, N, Δx,energy_sign)
                     os+= numerical_factor,ω[j],B[2],"Id",i,"Sy",j
                     os+= numerical_factor,ω[j],B[3],"Id",i,"Sz",j
                 else
-                    numerical_factor = (1/(N-1))
+                    numerical_factor = (1/(N_sites-1))
                     os+= numerical_factor,ω[i],B[1],"Sx",i,"Id",j  
                     os+= numerical_factor,ω[i],B[2],"Sy",i,"Id",j 
                     os+= numerical_factor,ω[i],B[3],"Sz",i,"Id",j 
