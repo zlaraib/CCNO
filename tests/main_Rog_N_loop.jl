fd82d33793dbd6bf520f67bdf067f492d02fef57
zlaraib <zlaraib@vols.utk.edu>
2024-02-05 17:15:10 -0500

    renamed N to N_sites

--- tests/main_Rog_N_loop.jl
+++ tests/main_Rog_N_loop.jl
@@ -8,25 +8,25 @@ include("../src/constants.jl")
 # Hamiltonian and then plots survival probability for 
 # multiple system sizes through loops. 
 
-function main(N)
+function main(N_sites)
     cutoff = 1E-14
     τ = 0.05
     ttotal = 5
     tolerance  = 5E-1
     Δx = 1E-3
-    s = siteinds("S=1/2", N; conserve_qns=false)
+    s = siteinds("S=1/2", N_sites; conserve_qns=false)
     a_t = 0.965
     b_t = 0
     c_t = 0
-    mu = ones(N)
-    n = mu .* fill((Δx)^3/(sqrt(2) * G_F), N)
+    mu = ones(N_sites)
+    n = mu .* fill((Δx)^3/(sqrt(2) * G_F), N_sites)
     B = [0, 0, -1]
-    ω_a = fill(2, div(N, 2))
-    ω_b = fill(0, div(N, 2))
+    ω_a = fill(2, div(N_sites, 2))
+    ω_b = fill(0, div(N_sites, 2))
     Δω = (ω_a - ω_b)/2
     ω = vcat(ω_a, ω_b)
-    ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up")
-    Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, ttotal)
+    ψ = productMPS(s, n -> n <= N_sites/2 ? "Dn" : "Up")
+    Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N_sites, Δx, ψ, cutoff, ttotal)
     function find_first_local_minima_index(arr)
         n = length(arr)
         for i in 2:(n-1)
@@ -45,20 +45,20 @@ function main(N)
     end
     t_min = τ * i_first_local_min - τ
     println("Corresponding time of first minimum index= ", t_min)
-    t_p_Rog = a_t*log(N) + b_t * sqrt(N) + c_t
+    t_p_Rog = a_t*log(N_sites) + b_t * sqrt(N_sites) + c_t
     println("t_p_Rog= ",t_p_Rog)
     # Check that our time of first minimum survival probability compared to Rogerro(2021) remains within the timestep and tolerance.
     @assert abs(t_min - t_p_Rog) <  τ + tolerance 
 
-    plot!(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probability p(t)", title = "Running Rog_particle_loop script", aspect_ratio=:auto, margin= 10mm, legend= true, label= ["My_plot_for_N$(N)"])
-    scatter!([t_p_Rog],[prob_surv_array[i_first_local_min]], label= ["t_p_Rog_for_N$(N)"])
-    scatter!([t_min],[prob_surv_array[i_first_local_min]], label= ["My_t_min__for_N$(N)"], legendfontsize=5, legend=:topright)
+    plot!(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probability p(t)", title = "Running Rog_particle_loop script", aspect_ratio=:auto, margin= 10mm, legend= true, label= ["My_plot_for_N$(N_sites)"])
+    scatter!([t_p_Rog],[prob_surv_array[i_first_local_min]], label= ["t_p_Rog_for_N$(N_sites)"])
+    scatter!([t_min],[prob_surv_array[i_first_local_min]], label= ["My_t_min__for_N$(N_sites)"], legendfontsize=5, legend=:topright)
     
 end
 
 # Loop from 4 to 8 particles with an increment of 4 particles each time
-for N in 4:4:8
-    @time main(N)
+for N_sites in 4:4:8
+    @time main(N_sites)
 end
 
 savefig("Survival probability vs t (Rog)_loop.pdf")
