75d9dc65e8892667b833b06a0114029789450659
zlaraib <zlaraib@vols.utk.edu>
2024-02-05 17:17:28 -0500

    renamed N to N_sites

--- tests/main_vac_osc.jl
+++ tests/main_vac_osc.jl
@@ -5,12 +5,12 @@ using LinearAlgebra
 include("../src/evolution.jl")
 include("../src/constants.jl")
 
-# We are simulating the time evolution of a 1D spin chain with N sites, where each site is a spin-1/2 particle. 
+# We are simulating the time evolution of a 1D spin chain with N_sites sites, where each site is a spin-1/2 particle. 
 # The simulation is done by applying a sequence of unitary gates to an initial state of the system, 
 # which is a product state where each site alternates between up and down.
 
 function main()
-  N = 4 # number of sites
+  N_sites = 4 # number of sites
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation
   τ = 0.1 # time step 
   ttotal = 5.0 # total time of evolution 
@@ -19,38 +19,38 @@ function main()
 
   # Make an array of 'site' indices and label as s 
   # conserve_qns=false doesnt conserve the total spin quantum number "S"(in z direction) in the system as it evolves
-  s = siteinds("S=1/2", N; conserve_qns=false)  
+  s = siteinds("S=1/2", N_sites; conserve_qns=false)  
 
-  # Initialize an array of zeros for all N particles
-  mu = zeros(N)
+  # Initialize an array of zeros for all N_sites particles
+  mu = zeros(N_sites)
                                 
-  # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F). This is the number of neutrinos.
-  n = mu.* fill((Δx)^3/(sqrt(2) * G_F), N)
+  # Create an array of dimension N_sites and fill it with the value 1/(sqrt(2) * G_F). This is the number of neutrinos.
+  n = mu.* fill((Δx)^3/(sqrt(2) * G_F), N_sites)
       
-  # Create a B vector which would be same for all N particles 
+  # Create a B vector which would be same for all N_sites particles 
   B = [1, 0, 0]          
   
   # Create arrays ω_a and ω_b
-  ω_a = fill(π, div(N, 2))
-  ω_b = fill(π, div(N, 2))
+  ω_a = fill(π, div(N_sites, 2))
+  ω_b = fill(π, div(N_sites, 2))
 
-  # Concatenate ω_a and ω_b to form ω with N elements. Each element of the array is a const pi.
+  # Concatenate ω_a and ω_b to form ω with N_sites elements. Each element of the array is a const pi.
   ω = vcat(ω_a, ω_b)
 
-  gates = create_gates(s, n, ω, B, N, Δx, τ)
+  gates = create_gates(s, n, ω, B, N_sites, Δx, τ)
   
   # Initialize psi to be a product state (First half to be spin down and other half to be spin up)
-  ψ = productMPS(s, n -> n <= N/2 ? "Dn" : "Up")
+  ψ = productMPS(s, n -> n <= N_sites/2 ? "Dn" : "Up")
 
   #extract output from the expect.jl file where the survival probability values were computed at each timestep
-  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N, Δx, ψ, cutoff, ttotal)
+  Sz_array, prob_surv_array = evolve(s, τ, n, ω, B, N_sites, Δx, ψ, cutoff, ttotal)
   
   expected_sz_array = Float64[]
   expected_sz= Float64[]
   
   for t in 0.0:τ:ttotal
 
-    for i in 1:(N-1)
+    for i in 1:(N_sites-1)
 
         if ω[i] != 0
 
