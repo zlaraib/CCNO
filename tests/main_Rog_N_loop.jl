42edf5a1291a6c6a90d04a8f6731e9c97e8f196b
zlaraib <zlaraib@vols.utk.edu>
2024-03-09 23:48:26 -0500

    added maxdim argument and saving datafiles and plots

--- tests/main_Rog_N_loop.jl
+++ tests/main_Rog_N_loop.jl
@@ -14,6 +14,7 @@ function main(N_sites)
     ttotal = 5
     tolerance  = 5E-1
     Δx = 1E-3
+    maxdim = 1000 #bond dimension
     s = siteinds("S=1/2", N_sites; conserve_qns=false)
     a_t = 0.965
     b_t = 0
@@ -27,7 +28,10 @@ function main(N_sites)
     ω = vcat(ω_a, ω_b)
     ψ = productMPS(s, N -> N <= N_sites/2 ? "Dn" : "Up")
     energy_sign = [i <= N_sites ÷ 2 ? 1 : 1 for i in 1:N_sites]
-    Sz_array, prob_surv_array = evolve(s, τ, N, ω, B, N_sites, Δx, ψ, energy_sign, cutoff,ttotal)
+    # Specify the relative directory path
+    datadir = joinpath(@__DIR__, "..","misc","datafiles","Rog_N_loop", "par_"*string(N_sites), "tt_"*string(ttotal))
+    #extract output from the expect.jl file where the survival probability values were computed at each timestep
+    Sz_array, prob_surv_array = evolve(s, τ, N, ω, B, N_sites, Δx, ψ, energy_sign, cutoff, maxdim, datadir,ttotal)
 
     function find_first_local_minima_index(arr)
         N = length(arr)
@@ -52,6 +56,11 @@ function main(N_sites)
     # Check that our time of first minimum survival probability compared to Rogerro(2021) remains within the timestep and tolerance.
     @assert abs(t_min - t_p_Rog) <  τ + tolerance 
 
+    # Specify the relative directory path
+    plotdir = joinpath(@__DIR__, "..","misc","plots","Rog_N_loop", "par_"*string(N_sites), "tt_"*string(ttotal))
+
+    # check if a directory exists, and if it doesn't, create it using mkpath
+    isdir(plotdir) || mkpath(plotdir)
     plot!(0.0:τ:τ*(length(prob_surv_array)-1), prob_surv_array, xlabel = "t", ylabel = "Survival Probability p(t)", title = "Running Rog_particle_loop script", aspect_ratio=:auto, margin= 10mm, legend= true, label= ["My_plot_for_N$(N_sites)"])
     scatter!([t_p_Rog],[prob_surv_array[i_first_local_min]], label= ["t_p_Rog_for_N$(N_sites)"])
     scatter!([t_min],[prob_surv_array[i_first_local_min]], label= ["My_t_min__for_N$(N_sites)"], legendfontsize=5, legend=:topright)
@@ -63,4 +72,4 @@ for N_sites in 4:4:8
     @time main(N_sites)
 end
 
-savefig("Survival probability vs t (Rog)_loop.pdf")
+savefig(joinpath(plotdir,"Survival probability vs t (Rog)_loop.pdf"))
