9441f95b539455c1db55aa5a34e0022e21e86024
zlaraib <zlaraib@vols.utk.edu>
2024-03-16 15:13:30 -0400

    too many changes to match with the initial conditions in richers test 2

--- tests/Bipolar_Osc_Richers.jl
+++ tests/Bipolar_Osc_Richers.jl
@@ -5,41 +5,48 @@ using LinearAlgebra
 using DelimitedFiles
 using Statistics
 
-#Needs revision
+include("main_Hamiltonian.jl")
+include("../src/constants.jl")
 
-include("main_self_interaction.jl")
-include("../src/constants")
-# dk what bipolar osc time is in the test
 """ Richers(2021) Test 2 initial conditions: """
-N_sites_eachflavor= 1 # total sites/particles that evenly spaced "for each (electron) flavor" 
+N_sites_eachflavor= 2 # total sites/particles that evenly spaced "for each (electron) flavor" 
 N_sites = 2* (N_sites_eachflavor) # total particles/sites for all neutrino and anti neutrino electron flavored
-τ = 2E-13 # time step to include 50 steps every 10 picoseconds # sec # variable
-ttotal = 9E-11 # total time of evolution # sec #variable
+τ = 0.05 # time step to include 50 steps every 10 picoseconds # sec # variable
+ttotal = 30 # total time of evolution # sec #variable
 tolerance  = 5E-1 # acceptable level of error or deviation from the exact value or solution #variable
-m1 = 8.6e3 #eV  1st mass eigenstate of neutrino
-m2 = 0 #eV  2nd mass eigenstate of neutrino
-#Δm² = (m2^2-m1^2) # mass square difference # (erg^2) #not used in the test
+m1 = -8.6e-3*eV  #1st mass eigenstate of neutrino #ergs
+# m1 =  -1.378e-14#ergs
+m2 = 0*eV  #2nd mass eigenstate of neutrino #ergs
+Δm² = (m2^2-m1^2) # mass square difference # (erg^2) #not used in the test
 #add other parameters like charcteristic bipolar time and H neu/ H vac =10  ...?
 maxdim = 1 # max bond dimension in MPS truncation
-cutoff = 0 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT) #variable
-L = 1 # cm # domain size # (aka big box length)
-n_νₑ =  (10* (m2-m1)^2 *c^4)/(2*√2*G_F*Eνₑ) # cm^-3 # number density of electron flavor neutrino
-n_νₑ̄ =  n_νₑ # cm^-3 # number density of electron flavor antineutrino
-Eνₑ =  50.0e6 # energy of all neutrinos (P.S the its negative is energy of all antineutrinos)
+cutoff = 1e-100 # specifies a truncation threshold for the SVD in MPS representation (SMALL CUTOFF = MORE ENTANGLEMENT) #variable
+L = 1e7 # cm # domain size # (aka big box length)
+# Δx = 1E-3 # length of the box of interacting neutrinos at a site in cm  # variable.
+Δx = L 
+Eνₑ =  50.0*MeV # energy of all neutrinos (P.S the its negative is energy of all antineutrinos) #ergs
+# Eνₑ = 8.011e-5 #ergs
 Eνₑ̄ = -1 * Eνₑ # specific to my case only. Since all neutrinos have same energy, except in my case anti neutrinos are moving in opposite direction to give it a negative sign
+# Eνₑ̄ = -10.0e6
+n_νₑ =  (10* (m2-m1)^2 )/(2*(√2)*G_F*Eνₑ) # cm^-3 # number density of electron flavor neutrino
+n_νₑ̄ =  n_νₑ # cm^-3 # number density of electron flavor antineutrino
+# n_νₑ̄ = (10* (m2-m1)^2 *c^4)/(2*√2*G_F*Eνₑ̄)
 #Select a shape function based on the shape_name variable form the list defined in dictionary in shape_func file
-shape_name = "triangular"  # Change this to the desired shape name #variable 
-Δp = 1/N_sites_eachflavor # width of shape function  # cm #variable
+shape_name = "none"  # Change this to the desired shape name #variable 
+# Δp = 1/N_sites_eachflavor # width of shape function  # cm #variable
+Δp = L # width of shape function  # cm #variable
 periodic = true  # true = imposes periodic boundary conditions while false doesn't
-
+theta_nu= 0.01 #0.5986 #rad # =34.3 degrees
+B = [sin(2 *theta_nu), 0, -cos(2*theta_nu)]
+B = B / norm(B) 
 # generate x_array such that the first particle is at position L/(2*N_sites) while subsequent particles are at a position incremental by L/N_sites. # grid style
 function generate_x_array(N_sites, L)
     return [(i - 0.5) * L / N_sites for i in 1:N_sites]
 end
 
 x = generate_x_array(N_sites, L)
-y = fill(rand(), N_sites) #variable
-z = fill(rand(), N_sites) #variable
+y = fill(0, N_sites) #variable
+z = fill(0, N_sites) #variable
 
 #generate a momentum array that depicts the energy of neutrinos and anti-neutrinos in opposing beams
 function generate_p_array(N_sites)                                                                                                                                                                                   
@@ -48,7 +55,8 @@ function generate_p_array(N_sites)
 end
 
 # p matrix with numbers generated from the p_array for all components (x, y, z)
-p = hcat(generate_p_array(N_sites), generate_p_array(N_sites), generate_p_array(N_sites))
+# p = hcat(generate_p_array(N_sites), generate_p_array(N_sites), generate_p_array(N_sites))
+p = hcat(generate_p_array(N_sites),fill(0, N_sites), fill(0, N_sites))
 # Create an array with the first half as 1 and the rest as -1
 energy_sign = [i <= N_sites ÷ 2 ? 1 : -1 for i in 1:N_sites] # half sites are (e) neutrinos with positive 1 entry while other half is anti (e) neutrinos with negative 1 entry
 
@@ -61,6 +69,7 @@ energy_sign = [i <= N_sites ÷ 2 ? 1 : -1 for i in 1:N_sites] # half sites are (
 s = siteinds("S=1/2", N_sites; conserve_qns=false) #fixed #switched conserve_qns to false to avoid fluxes error in expect function
 
 # Initialize psi to be a product state (Of all electron flavor neutrino i.e. spin up)
-ψ = productMPS(s, N_sites -> "Up") 
-
-@time main(N_sites_eachflavor,τ,ttotal,tolerance,Δm²,maxdim,cutoff,x, p, ψ, L,n_νₑ,n_νₑ̄,Eνₑ,Eνₑ̄,B_pert,B,shape_name,periodic)
+ψ₀= productMPS(s, N -> N <= N_sites/2 ? "Dn" : "Up")
+# ψ₀= productMPS(s, N_sites -> "Up") 
+@time main(s, τ, B,L, N_sites, N_sites_eachflavor, tolerance,
+n_νₑ,n_νₑ̄,Eνₑ,Eνₑ̄,Δx,Δm², p, x, Δp, ψ₀, shape_name, energy_sign, cutoff, maxdim, ttotal,periodic)
