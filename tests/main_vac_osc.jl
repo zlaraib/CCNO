c672f83dc49d906d9a74b381cba391d8fd15fda8
zlaraib <zlaraib@vols.utk.edu>
2024-03-16 15:16:29 -0400

    increased bond dimension and changed loop to cconver just the first site in assert

--- tests/main_vac_osc.jl
+++ tests/main_vac_osc.jl
@@ -15,13 +15,14 @@ include("../src/momentum.jl")
 
 function main()
   N_sites = 6 # number of sites, #variable
+  N_sites = 6 # number of sites, #variable
   cutoff = 1E-14 # specifies a truncation threshold for the SVD in MPS representation #variable
   τ = 0.1 # time step # sec #variable
   ttotal = 5.0 # total time of evolution #sec #variable
   Δx = 1E-3 # length of the box of interacting neutrinos at a site in cm #variable
   tolerance  = 1E-5 # acceptable level of error or deviation from the exact value or solution #variable
   Δm² = 2*π # Fixed for this vacuum oscillation case for omega =pi. dont change it to keep consistent results. 
-  maxdim = 1 # max bond dimension in MPS truncation
+  maxdim = 1000 # max bond dimension in MPS truncation
   L = 1 # cm # not being used in this test but defined to keep the evolve function arguments consistent.
   Δp = L # width of shape function # not being used in this test but defined to keep the evolve function arguments consistent.
 
@@ -29,12 +30,12 @@ function main()
   # conserve_qns=false doesnt conserve the total spin quantum number "S"(in z direction) in the system as it evolves
   s = siteinds("S=1/2", N_sites; conserve_qns=false)  #Fixed
 
-  # Initialize an array of zeros for all N particles
+  # Initialize an array of zeros for all N_sites particles
   mu = zeros(N_sites) #Fixed
                                 
   # Create an array of dimension N and fill it with the value 1/(sqrt(2) * G_F). This is the number of neutrinos.
   N = mu.* fill((Δx)^3/(sqrt(2) * G_F), N_sites) 
-      
+  
   # Create a B vector which would be same for all N particles 
   B = [1, 0, 0] # variable. But only other case that can be tested from this file is B = [0,0,-1].
 
@@ -64,24 +65,17 @@ function main()
   
   for t in 0.0:τ:ttotal
 
-    for i in 1:(N_sites-1)
+    i = 1 # change it according to the corresponding site number in the expect function 
+    if B[1] == 1
 
-        if ω[i] != 0
+      # Compute the expected value based on the derived analytic formula
+      expected_sz = -0.5 * cos(ω[i] * t)
 
-          if B[1] == 1
+    end
+    if B[3] == -1
 
-            # Compute the expected value based on the derived analytic formula
-            expected_sz = -0.5 * cos(ω[i] * t)
-
-          end
-          if B[3] == -1
-
-            # Compute the expected value based on the derived analytic formula
-            expected_sz = -0.5
-
-          end
-
-        end
+      # Compute the expected value based on the derived analytic formula
+      expected_sz = -0.5
 
     end
 
@@ -93,10 +87,10 @@ function main()
   # for B vector in x, it checks that the value of Sz at the first spin site oscillates between -0.5 and 0.5 
   # for B vector in -z, it checks that the value of Sz at the firstspin site never oscillates from -0.5 
   @assert all(abs.(Sz_array .- expected_sz_array) .< tolerance)
-
+  
   # Specify the relative directory path
   plotdir = joinpath(@__DIR__, "..","misc","plots","vac_osc", "par_"*string(N_sites), "tt_"*string(ttotal))
-    
+  
   # check if a directory exists, and if it doesn't, create it using mkpath
   isdir(plotdir) || mkpath(plotdir)
   plot(0.0:τ:τ*(length(Sz_array)-1), Sz_array, xlabel = "t", ylabel = "<Sz>", title = "Running main_vac_osc script",
