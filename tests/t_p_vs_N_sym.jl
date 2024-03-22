a279f6bd3123982b0639a1ebe309e3be4b0d1fe3
zlaraib <zlaraib@vols.utk.edu>
2024-03-22 00:12:39 -0400

    making Rog_sym_omega file consistent with more parameters

--- tests/t_p_vs_N_sym.jl
+++ tests/t_p_vs_N_sym.jl
@@ -23,7 +23,11 @@ function main(N_sites, Δω)
     ttotal = 10
     tolerance  = 5E-1
     Δx = 1E-3
+    Δm² = Δω
     maxdim = 1000 #bond dimension
+    L = 10 # cm # not being used in this test but defined to keep the evolve function arguments consistent.
+    Δp = L # width of shape function # not being used in this test but defined to keep the evolve function arguments consistent.  
+    periodic = false
     s = siteinds("S=1/2", N_sites; conserve_qns=false)
     # check for Δω = 0.25
     a_t = 1.224
@@ -31,17 +35,25 @@ function main(N_sites, Δω)
     c_t = 1.62
     mu = ones(N_sites)
     N = mu .* fill(((Δx)^3 )/(√2 * G_F * N_sites), N_sites)
-    B = [0, 0, -1]
-    Δω_array= fill(Δω, div(N_sites, 2))
-    # Calculate ω_a and ω_b based on Δω
-    ω_a = Δω_array 
-    ω_b = -Δω_array 
-    
-    ω = vcat(ω_a, ω_b)
+    theta_nu = 0 # mixing_angle #rad 
+    B = [sin(2*theta_nu), 0, -cos(2*theta_nu)] # is equivalent to B = [0, 0, -1] # fixed for Rogerro's case
+    B = B / norm(B)
+    function generate_p_array(N_sites)                                                                                                                                                                                   
+        half_N_sites = div(N_sites, 2)
+        return [fill(1, half_N_sites); fill(1, half_N_sites)]
+    end
+    # p matrix with numbers generated from the p_array for all components (x, y, z)
+    p = hcat(generate_p_array(N_sites),fill(0, N_sites), fill(0, N_sites))
+    x = fill(rand(), N_sites) # variable.
+    y = fill(rand(), N_sites) # variable.
+    z = fill(rand(), N_sites) # variable.
+
     ψ = productMPS(s, N -> N <= N_sites/2 ? "Dn" : "Up")
     energy_sign = [i <= N_sites ÷ 2 ? 1 : 1 for i in 1:N_sites]
+    shape_name = "none" 
     datadir = joinpath(@__DIR__, "..","misc","datafiles","Rog_Fig_3b", "par_"*string(N_sites), "tt_"*string(ttotal))
-    Sz_array, prob_surv_array =  evolve(s, τ, N, ω, B, N_sites, Δx, ψ, energy_sign, cutoff, maxdim, datadir,ttotal)
+    Sz_array, Sy_array, Sx_array, prob_surv_array, x_values, pₓ_values, ρₑₑ_array,ρ_μμ_array= evolve(s, τ, N, B,L, N_sites, 
+    Δx,Δm², p, x, Δp, theta_nu, ψ, shape_name, energy_sign, cutoff, maxdim, datadir, ttotal,periodic)
     function find_first_local_minima_index(arr)
         N = length(arr)
         for i in 2:(N-1)
