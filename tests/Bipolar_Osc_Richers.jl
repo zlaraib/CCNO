c3c0c6b82b4b77c52ca137cca50851c87e0dac24
zlaraib <zlaraib@vols.utk.edu>
2024-09-12 13:02:52 -0400

    not saving data and plots in jenkins unit tests, decoposed cintents of hamiltonian file to separate directory files in utilities and initializations

--- tests/Bipolar_Osc_Richers.jl
+++ tests/Bipolar_Osc_Richers.jl
@@ -5,8 +5,36 @@ using LinearAlgebra
 using DelimitedFiles
 using Statistics
 
-include("main_Hamiltonian.jl")
-include("../src/constants.jl")
+"""
+For github unit tests runs: 
+src_dir = ../
+save_data and save_plots_flag should be false to run test files. 
+"""
+
+src_dir = "../"
+save_data = false  # true = saves datafiles for science runs while false doesn't. So change it to false for jenkins test runs
+save_plots_flag = false # true = saves plots for science runs while false doesn't. So change it to false for jenkins test runs
+
+
+"""
+For science runs: 
+src_dir = /home/zohalaraib/Oscillatrino/ # should be changed to users PATH
+save_data and save_plots_flag should be true to run test files. 
+
+"""
+# src_dir= "/home/zohalaraib/Oscillatrino/" # should be changed to users PATH
+# save_data = true  # true = saves datafiles for science runs while false doesn't. So change it to false for jenkins test runs
+# save_plots_flag = true # true = saves plots for science runs while false doesn't. So change it to false for jenkins test runs
+    
+
+include(src_dir * "src/evolution.jl")
+include(src_dir * "src/constants.jl")
+include(src_dir * "src/shape_func.jl")
+include(src_dir * "src/momentum.jl")
+include(src_dir * "src/perturb.jl")
+include(src_dir * "Initializations/initial_cond.jl")
+include(src_dir * "Utilities/gen_input_file.jl")
+include(src_dir * "Utilities/save_plots.jl")
 
 """ Richers(2021) Test 2 initial conditions: """
 N_sites_eachflavor= 1 # total sites/particles that evenly spaced "for each (electron) flavor" 
@@ -35,23 +63,13 @@ periodic = true  # true = imposes periodic boundary conditions while false doesn
 theta_nu= 0.01 #mixing angle # =34.3 degrees
 B = [-sin(2 *theta_nu), 0, cos(2*theta_nu)] # for inverted mass hierarchy
 B = B / norm(B) 
-# generate x_array such that the first particle is at position L/(2*N_sites) while subsequent particles are at a position incremental by L/N_sites. # grid style
-function generate_x_array(N_sites, L)
-    return [(i - 0.5) * L / N_sites for i in 1:N_sites]
-end
 
 x = generate_x_array(N_sites, L)
 y = fill(0, N_sites) #variable
 z = fill(0, N_sites) #variable
 
-#generate a momentum array that depicts the energy of neutrinos and anti-neutrinos in opposing beams
-function generate_p_array(N_sites)                                                                                                                                                                                   
-    half_N_sites = div(N_sites, 2)
-    return [fill(Eνₑ, half_N_sites); fill(Eνₑ̄, half_N_sites)]
-end
+p = hcat(generate_px_array(N_sites), generate_py_array(N_sites), generate_pz_array(N_sites))
 
-# p matrix with numbers generated from the p_array for all components (x, y, z)
-p = hcat(generate_p_array(N_sites),fill(0, N_sites), fill(0, N_sites))
 # Create an array with the first half as 1 and the rest as -1
 energy_sign = [i <= N_sites ÷ 2 ? -1 : 1 for i in 1:N_sites] # half sites are (e) neutrinos with positive 1 entry while other half is anti (e) neutrinos with negative 1 entry
 
@@ -66,5 +84,40 @@ s = siteinds("S=1/2", N_sites; conserve_qns=false) #fixed #switched conserve_qns
 # Initialize psi to be a product state (Of first half electron flavor neutrino i.e. spin up while other half anti-neutrinos electron flavor i.e. half spin down)
 ψ₀= productMPS(s, N -> N <= N_sites/2 ? "Up" : "Dn")
 
-@time main(s, τ, B,L, N_sites, N_sites_eachflavor, tolerance,
-n_νₑ,n_νₑ̄,Eνₑ,Eνₑ̄,Δx,Δm², p, x, Δp, ψ₀, shape_name, energy_sign, cutoff, maxdim, ttotal,periodic)
+# @time main(s, τ, B,L, N_sites, N_sites_eachflavor, tolerance,
+# n_νₑ,n_νₑ̄,Eνₑ,Eνₑ̄,Δx,Δm², p, x, Δp, ψ₀, shape_name, energy_sign, cutoff, maxdim, ttotal,periodic)
+N = Neutrino_number(s, τ, B,L, N_sites, N_sites_eachflavor, tolerance,
+                n_νₑ,n_νₑ̄,Eνₑ,Eνₑ̄,Δx,Δm², p, x, Δp, ψ₀, shape_name, energy_sign, cutoff, maxdim, ttotal,periodic)
+
+# Specify the relative directory path
+datadir = joinpath(@__DIR__,"datafiles", "Richers", "par_"*string(N_sites), "tt_"*string(ttotal))
+
+#extract output for the survival probability values at each timestep
+Sz_array, Sy_array, Sx_array, prob_surv_array, x_values, pₓ_values, ρₑₑ_array, ρ_μμ_array, ρₑμ_array, Im_Ω = evolve(
+    s, τ, N, B, L, N_sites, Δx, Δm², p, x, Δp, theta_nu, ψ₀, shape_name, energy_sign, cutoff, maxdim, datadir, t1, t2, ttotal, save_data , periodic)
+
+# insert assert condition here 
+
+if save_data
+    # Generate input data
+    input_data = extract_initial_conditions()
+
+    # Call the function to generate the inputs file in the specified directory
+    generate_inputs_file(datadir, "inputs.txt", input_data)
+end
+
+if save_plots_flag 
+    # Specify the relative directory path
+    plotdir = joinpath(@__DIR__, "plots", "Richers", "par_"*string(N_sites), "tt_"*string(ttotal))  
+
+    save_plots(τ, N_sites, ttotal,Sz_array, Sy_array, Sx_array, prob_surv_array, x_values, pₓ_values, ρₑₑ_array,ρ_μμ_array, ρₑμ_array, plotdir, save_plots_flag)
+    
+    # Call the function to generate the inputs file in the specified directory
+    generate_inputs_file(plotdir, "inputs.txt", input_data)
+end
+
+# Plotting ρ_ee vs t # for jenkins file 
+plot(0.0:τ:τ*(length(ρₑₑ_array)-1), ρₑₑ_array, xlabel = "t", ylabel = "<ρₑₑ>", legend = false, 
+left_margin = 20mm, right_margin = 10mm, top_margin = 5mm, bottom_margin = 10mm) 
+# Save the plot as a PDF file
+savefig("Bipolar Richers for $N_sites particles <ρₑₑ>_vs_t.pdf")
