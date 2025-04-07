41ed0411cb8e28b1b7b209129605976369df2f6d
Sherwood Richers <richers@utk.edu>
2025-04-07 09:27:25 -0400

    optimize shape function

--- src/physics/shape_func.jl
+++ src/physics/shape_func.jl
@@ -24,28 +24,23 @@ const shapes = Dict(
     
 @inline function shape_func(params::CCNO.Parameters, x::Vector{Float64}, i::Int, j::Int,L::Float64)
 
-    if haskey(shapes, params.shape_name)
-        shape_function = shapes[params.shape_name]  # assign the corresponding function (selected in shape_name in the test file) to shape_function.
-        # ξ determines the overlap of neutrinos (i.e. their shapes) on top of each other to signify the neutrino-neutrino interaction strength.
-        ξ = (x[i] - x[j]) / params.Δp # dependent upon difference in distance of i and j sites and the width of the shape function. 
-        if params.periodic 
-            if x[i]-x[j] > L/2
-                ξ = ξ - L/params.Δp 
-            end
-            if x[i] - x[j] < -L/2
-                ξ = ξ + L/params.Δp 
-            end
-
-            if params.shape_name == "none"
-                @assert (params.Δp == L)
-            else @assert (2*params.Δp <L)
-            end
-            @assert (abs(ξ)<= L/(2* params.Δp))
+    shape_function = shapes[params.shape_name]  # assign the corresponding function (selected in shape_name in the test file) to shape_function.
+    # ξ determines the overlap of neutrinos (i.e. their shapes) on top of each other to signify the neutrino-neutrino interaction strength.
+    ξ::Float64 = (x[i] - x[j]) / params.Δp # dependent upon difference in distance of i and j sites and the width of the shape function. 
+    if params.periodic 
+        if x[i]-x[j] > L/2
+            ξ = ξ - L/params.Δp 
         end
-
-        return shape_function(params.Δp, ξ)
-    else
-        error("Unknown shape: $params.shape_name")
+        if x[i] - x[j] < -L/2
+            ξ = ξ + L/params.Δp 
+        end
+        
+        if params.shape_name == "none"
+            @assert (params.Δp == L)
+        else @assert (2*params.Δp <L)
+        end
+        @assert (abs(ξ)<= L/(2* params.Δp))
     end
-
+    
+    return shapes[params.shape_name](params.Δp, ξ)
 end
