55030bf6ec6d702289395863339784ee2c0ebd67
Sherwood Richers <richers@utk.edu>
2025-04-06 22:10:50 -0400

    declare constants as const

--- src/physics/constants.jl
+++ src/physics/constants.jl
@@ -1,7 +1,7 @@
-hbar = 1.05457266e-27 # erg s
-c = 2.99792458e10 # cm/s
-eV = 1.60218e-12 # erg
-MeV = 1e6 * eV
-GeV = 1e9 * eV
-G_F = 1.1663787e-5 / GeV^2 * (hbar*c)^3 # erg cm^3
-kB = 1.3807e-16 # erg/K
+const hbar = 1.05457266e-27 # erg s
+const c = 2.99792458e10 # cm/s
+const eV = 1.60218e-12 # erg
+const MeV = 1e6 * eV
+const GeV = 1e9 * eV
+const G_F = 1.1663787e-5 / GeV^2 * (hbar*c)^3 # erg cm^3
+const kB = 1.3807e-16 # erg/K
