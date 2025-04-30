e8312a63808ad8d84a473033021ea722baa3237d
zlaraib <zlaraib@vols.utk.edu>
2025-04-30 17:14:52 -0400

    fixed functional argumnets, added tau time stepping for correct recover. Removed extra arguments not requiring reading

--- src/utilities/checkpoint_simulation_hdf5.jl
+++ src/utilities/checkpoint_simulation_hdf5.jl
@@ -41,16 +41,16 @@ function checkpoint_simulation_hdf5(params::CCNO.Parameters, checkpoint_filename
     println("Checkpoint created at $checkpoint_filename, iteration $iteration, time $t")
 end
 
-function recover_checkpoint_hdf5(checkpoint_filename::String)
+function recover_checkpoint_hdf5(params::CCNO.Parameters, checkpoint_filename::String)
     # Open the HDF5 file for reading
     f = h5open(checkpoint_filename, "r")
     
     # Read scalar values with units
-    τ = read(f, "τ(sec)")
-    L = read(f, "L(cm)")
-    Δx = read(f, "Δx(cm)")
-    m1 = read(f, "m1(erg)")
-    m2 = read(f, "m2(erg)")
+    # τ = read(f, "τ(sec)")
+    # L = read(f, "L(cm)")
+    # Δx = read(f, "Δx(cm)")
+    # m1 = read(f, "m1(erg)")
+    # m2 = read(f, "m2(erg)")
     t_initial = read(f, "t(sec)")
     iteration = read(f, "iteration(unitless const)")
     
@@ -74,7 +74,7 @@ function recover_checkpoint_hdf5(checkpoint_filename::String)
     println("Recovered checkpoint from $checkpoint_filename, iteration $iteration, time $t_initial")
 
     s = siteinds(ψ)
-    state = simulation_state(ψ=ψ, s=s, energy_sign=energy_sign, N=N, p=p, xyz=xyz)
-    
+    state = CCNO.SimulationState(ψ=ψ, s=s, energy_sign=energy_sign, N=N, p=p, xyz=xyz)
+    t_initial += params.τ
     return state, t_initial, iteration
 end
